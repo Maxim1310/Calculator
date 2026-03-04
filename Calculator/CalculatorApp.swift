34a2859f1e5ec007c601de3d857ec3ac47fe7805
Maxim1310 <makimr@internet.ru>
2026-03-04 14:53:20 +0200

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Maxik on 04.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
